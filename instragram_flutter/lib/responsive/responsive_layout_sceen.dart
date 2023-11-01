d0acae209f82b285009329779cacdbd54f54c3af
sajjadrahman56 <maxsajjad29@gmail.com>
2023-11-01 21:34:48 +0600

    error exist in the  android

---
+++ instragram_flutter/lib/responsive/responsive_layout_sceen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import '../utils/dimension.dart';
+
+class ResponsiveLayout extends StatelessWidget {
+  final Widget webScreenLayout;
+  final Widget mobileScreenLayout;
+  const ResponsiveLayout(
+      {super.key,
+      required this.mobileScreenLayout,
+      required this.webScreenLayout});
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(builder: (context, constraints) {
+      if (constraints.maxWidth > webScreenSize) {
+        return webScreenLayout;
+      }
+      return mobileScreenLayout;
+    });
+  }
+}
